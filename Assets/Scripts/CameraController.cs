80bf58157a81e31cfa7445a131079fda8480b378
ErikJungnickel <contact@backyard-dev.de>
2016-10-05 23:11:08 +0200

    Camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(20 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-20 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 20 * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -20 * Time.deltaTime, 0));
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) 
+        {
+            Camera.main.orthographicSize += 2;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Camera.main.orthographicSize -= 2;
+        }
+    }
 }
